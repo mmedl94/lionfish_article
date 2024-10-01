6016109051c6ed6fbc5fb22a1d98a36e782931ea
mmedl94 <matthias.medl@chello.at>
2024-10-01 23:05:47 +0200

    reworked figures 3, 7, 8, 9 and 10. minor adjustments to text. added scripts to load interfaces and recreate plots

--- scripts/load_snapshots.R
+++ scripts/load_snapshots.R
@@ -4,9 +4,20 @@ remotes::install_github("mmedl94/lionfish")
 library(lionfish)
 library(dplyr)
 
+library(tourr)
+library(devtools)
+library(reticulate)
+library(flexclust)
+library(stats)
+library(dplyr)
+setwd("/home/matthias/Desktop/Work/gsoc/lionfish")
+load_all()
+
+setwd("/home/matthias/Desktop/Work/gsoc/paper/pytour_paper")
 # set working directory
 setwd("..")
-
+setwd("/home/matthias/Desktop/Work/gsoc/paper/pytour_paper")
+init_env()
 # load Austrian Vacation Activities dataset
 data("winterActiv")
 winterActiv_features <- read.csv("saves/aut_saves/init/feature_selection.csv",
@@ -23,16 +34,21 @@ ausActiv <- ausActiv[, colnames(ausActiv) %in% ausActiv_features$V1]
 ausActiv <- ausActiv[, ausActiv_features$V1]
 
 # Figure 7 + figure 8
-load_interactive_tour(winterActiv, "/saves/aut_saves/init")
+load_interactive_tour(winterActiv, "/saves/aut_saves/init",
+                      display_size = 5)
 
 # Figure 9
-load_interactive_tour(winterActiv, "/saves/aut_saves/before")
+load_interactive_tour(winterActiv, "/saves/aut_saves/before",
+                      display_size = 5)
 
 # Figure 10
-load_interactive_tour(winterActiv, "/saves/aut_saves/after")
+load_interactive_tour(winterActiv, "/saves/aut_saves/after",
+                      display_size = 5)
 
 # Figure 12 + 13
-load_interactive_tour(ausActiv, "/saves/aus_saves/before")
+load_interactive_tour(ausActiv, "/saves/aus_saves/before",
+                      display_size = 5)
 
 # Figure 14
-load_interactive_tour(ausActiv, "/saves/aus_saves/after")
+load_interactive_tour(ausActiv, "/saves/aus_saves/after",
+                      display_size = 5)
