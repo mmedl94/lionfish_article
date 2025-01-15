654dfffc37d86992216a378200bdd946c95f9376
mmedl94 <matthias.medl@chello.at>
2025-01-15 15:15:01 +0100

    refinement of scripts

--- scripts/austrian_tourism.R
+++ scripts/austrian_tourism.R
@@ -12,27 +12,12 @@ set.seed(630)
 t1 <- save_history(winterActiv, max=20)
 t1i <- interpolate(t1)
 
-# Save projections to illustrate the binary nature
-proj <- matrix(t1i[,,3], nrow=27, ncol=2)
-proj <- matrix(t1i[,,60], nrow=27, ncol=2)
-proj <- matrix(t1i[,,30], nrow=27, ncol=2)
-p <- render_proj(winterActiv_std, proj)
-ggplot() +
-  geom_point(data=p$data_prj, aes(x=P1, y=P2)) +
-  #xlim(-1,1) + ylim(-1, 1) +
-  theme_bw() +
-  theme(aspect.ratio=1,
-        axis.text=element_blank(),
-        axis.title=element_blank(),
-        axis.ticks=element_blank(),
-        panel.grid=element_blank())
-
 proj1 <- matrix(t1i[,,1], nrow=27, ncol=2)
 proj2 <- matrix(t1i[,,3], nrow=27, ncol=2)
 proj3 <- matrix(t1i[,,10], nrow=27, ncol=2)
 proj4 <- matrix(t1i[,,30], nrow=27, ncol=2)
 
-p1 <- render_proj(winterActiv_std, proj1)
+p1 <- render_proj(winterActiv, proj1)
 plot1 <- ggplot() +
   geom_point(data=p1$data_prj, aes(x=P1, y=P2)) +
   theme_bw() +
@@ -42,7 +27,7 @@ plot1 <- ggplot() +
         panel.grid=element_blank()) +
   labs(x = "Alpine skiing", y = "Cross country skiing")
 
-p2 <- render_proj(winterActiv_std, proj2)
+p2 <- render_proj(winterActiv, proj2)
 plot2 <- ggplot() +
   geom_point(data=p2$data_prj, aes(x=P1, y=P2)) +
   theme_bw() +
@@ -52,7 +37,7 @@ plot2 <- ggplot() +
         panel.grid=element_blank()) +
   labs(x = "Projection 1", y = "Projection 2")
 
-p3 <- render_proj(winterActiv_std, proj3)
+p3 <- render_proj(winterActiv, proj3)
 plot3 <- ggplot() +
   geom_point(data=p3$data_prj, aes(x=P1, y=P2)) +
   theme_bw() +
@@ -62,7 +47,7 @@ plot3 <- ggplot() +
         panel.grid=element_blank()) +
   labs(x = "Projection 1", y = "Projection 2")
 
-p4 <- render_proj(winterActiv_std, proj4)
+p4 <- render_proj(winterActiv, proj4)
 plot4 <- ggplot() +
   geom_point(data=p4$data_prj, aes(x=P1, y=P2)) +
   theme_bw() +
